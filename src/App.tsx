22e965bf5d707c3ba646738ee9a871e4d7275061
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-06-30 21:25:10 +0200

    App.tsx: add TimerOptions and HistoryButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import HistoryButton from "./components/HistoryButton";
+import TimerOptions from "./components/TimerOptions";
 import "./App.css";
 
 function App() {
-  return <p>Initial setup</p>;
+  const [historyIsOpened, setHistoryIsOpened] = useState(false);
+
+  return (
+    <div className="main-container" style={{ height: "400px" }}>
+      <div className="header">
+        <h1>Work Timer</h1>
+        <HistoryButton
+          onClick={() => setHistoryIsOpened((prev) => !prev)}
+          historyIsOpened={historyIsOpened}
+        />
+      </div>
+      <TimerOptions />
+    </div>
+  );
 }
 
 export default App;
