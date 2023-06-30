520189e13ae3fba1749b321808f22570a3e4c5e2
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-06-30 21:29:17 +0200

    Add HistoryButton.tsx

---
+++ src/components/HistoryButton.tsx
@@ -0,0 +1,24 @@
+import { SyntheticEvent } from "react";
+import ArrowDownIcon from "../icons/ArrowDownIcon";
+import classes from "./HistoryButton.module.css";
+
+type HistoryButtonType = {
+  onClick: (e: SyntheticEvent) => void;
+  historyIsOpened: boolean;
+};
+
+function HistoryButton({ onClick, historyIsOpened }: HistoryButtonType) {
+  return (
+    <button
+      className={`${classes.historyBtn} ${historyIsOpened ? classes.back : ""}`}
+      onClick={onClick}
+    >
+      <span>{historyIsOpened ? "back" : "history"}</span>
+      <div className={classes.iconContainer}>
+        <ArrowDownIcon />
+      </div>
+    </button>
+  );
+}
+
+export default HistoryButton;
