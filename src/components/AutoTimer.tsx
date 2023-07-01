96238c7667ada764955a0455a4a3934821dc1c12
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-01 23:44:46 +0200

    AutoTimer.tsx: add props to TimerCircle

--- src/components/AutoTimer.tsx
+++ src/components/AutoTimer.tsx
@@ -5,7 +5,7 @@ function AutoTimer() {
   return (
     <div className={classes.auto_timer_container}>
       <div className={classes.auto_timer}>
-        <TimerCircle width="150px" />
+        <TimerCircle width={150} strokeWidth={10} />
       </div>
       <div className={classes.actions}></div>
       <div className={classes.time_set}></div>
