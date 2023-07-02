ed43306623db6a23a4cfe8d571da7858680fd40c
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-02 23:18:32 +0200

    Circle.tsx: delete inline styles and add props

--- src/icons/Circle.tsx
+++ src/icons/Circle.tsx
@@ -1,35 +1,38 @@
 type CircleType = {
   width: number;
   strokeWidth: number;
+  strokeDashoffset: number;
 };
 
-function Circle({ width, strokeWidth }: CircleType) {
+function Circle({ width, strokeWidth, strokeDashoffset }: CircleType) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
       width={width}
       height={width}
-      style={{ transform: "rotate(-90deg)" }}
+      // style={{ transform: "rotate(-90deg)" }}
     >
       <defs>
         <linearGradient id="GradientColor">
-          <stop offset="0%" stop-color="#e91e63" />
-          <stop offset="100%" stop-color="#673ab7" />
+          <stop offset="0%" stopColor="#e91e63" />
+          <stop offset="100%" stopColor="#673ab7" />
         </linearGradient>
       </defs>
       <circle
-        cx="75"
-        cy="75"
-        r="70"
+        cx={`${width / 2}`}
+        cy={`${width / 2}`}
+        r={`${(width - strokeWidth) / 2}`}
         // stroke-linecap="round"
-        style={{
-          fill: "none",
-          stroke: "orange",
-          strokeWidth: `${strokeWidth}px`,
-          strokeDasharray: "472",
-          strokeDashoffset: "460",
-        }}
+        style={
+          {
+            // fill: "none",
+            // stroke: "orange",
+            // strokeWidth: `${strokeWidth}px`,
+            // strokeDasharray: `${strokeDashoffset}`,
+            // strokeDashoffset: `${(strokeDashoffset * 50) / 100}`,
+          }
+        }
       />
     </svg>
   );
