ca9056752c065ead28096cb4a655c4cea5df1b5d
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-02 23:16:51 +0200

    AutoTimer.tsx: change layout and add TimerInput and add styles

--- src/components/AutoTimer.tsx
+++ src/components/AutoTimer.tsx
@@ -1,14 +1,20 @@
 import classes from "./AutoTimer.module.css";
 import TimerCircle from "./TimerCircle";
+import TimeInput from "./UI/TimeInput";
 
 function AutoTimer() {
   return (
     <div className={classes.auto_timer_container}>
       <div className={classes.auto_timer}>
-        <TimerCircle width={150} strokeWidth={10} />
+        <TimerCircle width={180} strokeWidth={10} />
       </div>
-      <div className={classes.actions}></div>
-      <div className={classes.time_set}></div>
+      <div className={classes.time_set}>
+        <div className={classes.inputs_container}>
+          <TimeInput id="minutes" range={24} />
+          <span>:</span>
+          <TimeInput id="minutes" range={60} />
+        </div>
+      </div>
     </div>
   );
 }
