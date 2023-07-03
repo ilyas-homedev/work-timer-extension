9f7cb15ce32188e0f8bc75cc50e030f14186f07d
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-03 21:45:19 +0200

    TimerCircle.tsx: add styles

--- src/components/TimerCircle.tsx
+++ src/components/TimerCircle.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import classes from "./TimerCircle.module.css";
 import Circle from "../icons/Circle";
 import CircleAnimation from "./styled/CircleAnimation";
@@ -18,6 +18,13 @@ function TimerCircle({
   const innerCircleWidth = `${width - 2 * strokeWidth}px`;
   const strokeDashoffset = Math.PI * (width - strokeWidth);
 
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function startTimer() {
+    setTimerStarted(true);
+    // const timer = setTimeout(() => {}, 10000);
+  }
+
   return (
     <div
       style={{ width: width, height: width }}
@@ -27,21 +34,25 @@ function TimerCircle({
         width={width}
         strokeWidth={strokeWidth}
         strokeDashoffset={strokeDashoffset}
+        timeout={10}
       >
         <Circle
           width={width}
           strokeWidth={strokeWidth}
           strokeDashoffset={strokeDashoffset}
+          className={timerStarted ? "timer_start" : ""}
         />
       </CircleAnimation>
       <div
         style={{
           width: innerCircleWidth,
           height: innerCircleWidth,
+          top: strokeWidth,
+          left: strokeWidth,
         }}
         className={`${classes.inner_circle} ${className}`}
       >
-        {children}
+        <button onClick={startTimer}>start</button>
       </div>
     </div>
   );
