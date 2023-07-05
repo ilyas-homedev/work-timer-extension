86ed95b15f0d8a7fee68f95d365a11f80c8ea3de
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-05 21:37:18 +0200

    SetTime.tsx: add props

--- src/components/SetTime.tsx
+++ src/components/SetTime.tsx
@@ -2,17 +2,18 @@ import TimeInput from "./UI/TimeInput";
 import classes from "./SetTime.module.css";
 
 type SetTimeType = {
+  groupName: string;
   label: string;
 };
 
-function SetTime({ label }: SetTimeType) {
+function SetTime({ groupName, label }: SetTimeType) {
   return (
     <div className={classes.set_time_group}>
       <p className={classes.label}>{label}</p>
       <div className={classes.inputs_container}>
-        <TimeInput id="minutes" range={23} />
+        <TimeInput groupName={groupName} id="hours" range={23} />
         <span>:</span>
-        <TimeInput id="minutes" range={59} />
+        <TimeInput groupName={groupName} id="minutes" range={59} />
       </div>
     </div>
   );
