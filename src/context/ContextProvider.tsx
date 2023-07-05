1d2ea3204a8dfa5b42f440efc94251af299803a9
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-07-05 21:42:16 +0200

    Add ContextProvider.tsx

---
+++ src/context/ContextProvider.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren, createContext, useState } from "react";
+
+interface ITimeValues {
+  [work: string]: { [hours: string]: string; minutes: string };
+  rest: { hours: string; minutes: string };
+}
+
+export const TimerContext = createContext<
+  [ITimeValues, (value: string, groupName: string, unitOfTime: string) => void]
+>([
+  {
+    work: { hours: "00", minutes: "00" },
+    rest: { hours: "00", minutes: "00" },
+  },
+  () => {},
+]);
+
+function ContextProvider({ children }: PropsWithChildren) {
+  const [timeValues, setTimeValues] = useState<ITimeValues>({
+    work: { hours: "00", minutes: "00" },
+    rest: { hours: "00", minutes: "00" },
+  });
+
+  function setTime(value: string, groupName: string, unitOfTime: string) {
+    setTimeValues((prev) => {
+      return {
+        ...prev,
+        [groupName]: {
+          ...prev[groupName as keyof typeof timeValues],
+          [unitOfTime]: value,
+        },
+      };
+    });
+  }
+
+  console.log(timeValues);
+
+  return (
+    <TimerContext.Provider value={[timeValues, setTime]}>
+      {children}
+    </TimerContext.Provider>
+  );
+}
+
+export default ContextProvider;
